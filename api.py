8c6d5d7830ee8e100eddabe02be90767d390e97b
GlebsIT <belgberez@gmail.com>
2019-07-24 14:27:47 +0400

    update api.py

--- api.py
+++ api.py
@@ -76,7 +76,7 @@ def handle_dialog(req, res):
         return
     else:
         cur = conn.cursor()
-        cur.execute("SELECT * FROM messages WHERE session_id = 'teteee' ORDER BY message_id DESC LIMIT 1")
+        cur.execute("SELECT * FROM messages WHERE session_id = ? ORDER BY message_id DESC LIMIT 1", (req['session']['session_id']))
         results = cur.fetchall()
     # for row in results:
     #     logging.info('row: %r', row)
