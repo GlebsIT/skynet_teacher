3cc76c181639d3da9aea539d5601ba472845e205
GlebsIT <belgberez@gmail.com>
2019-07-24 13:54:14 +0400

    update api.py

--- api.py
+++ api.py
@@ -67,7 +67,7 @@ def handle_dialog(req, res):
 
         # Создание кнопок
         res['response']['buttons'] = get_suggests(user_id)
-        message.append(res['response']['text'])
+        #message.append(res['response']['text'])
         #with conn:
         #    create_message(conn, message)
         logging.info('message: %r', message)
